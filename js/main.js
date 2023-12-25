64cfa181081629b0035992f023324146a4b82fd7
Fymos <zufarovfayzullo703@gmail.com>
2023-12-26 00:33:32 +0500

    new commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 var elForm = document.querySelector(".form")
-var elFile = document.querySelector(".form__file").value
-var elName = document.querySelector(".form__name")
-var elPrice = document.querySelector(".form__price")
 var cardImg = document.querySelector(".card__img")
 var cardTitle = document.querySelector(".card__title")
 var cardText = document.querySelector(".card__text")
 
 elForm.addEventListener('submit', function(evnt) {
   evnt.preventDefault()
-  cardTitle.textContent = elName.value
-  cardText.textContent = elPrice.value + "$"
+  cardImg.src = URL.createObjectURL(evnt.target.img.files[0])
+  cardTitle.textContent = evnt.target.name.value
+  cardText.textContent = evnt.target.price.value + "$"
 })
